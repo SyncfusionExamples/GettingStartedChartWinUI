85075dfb88fc193b66431bf3d3d3a70f71d42144
Lavanya A <lavanya.anaimuthu@syncfusion.com>
2021-07-01 11:40:50 +0530

    changed viewmodels data.

--- CartesianChartGettingStarted/CartesianChartDesktop/CartesianChartDesktop/ViewModel/ViewModel.cs
+++ CartesianChartGettingStarted/CartesianChartDesktop/CartesianChartDesktop/ViewModel/ViewModel.cs
@@ -15,10 +15,11 @@ namespace CartesianChartDesktop
         {
             Data = new ObservableCollection<Person>()
             {
-                new Person { Name = "David", Height = 180 },
-                new Person { Name = "Michael", Height = 170 },
-                new Person { Name = "Steve", Height = 160 },
-                new Person { Name = "Joel", Height = 182 }
+                new Person { Name = "David", Height = 170 },
+                new Person { Name = "Michael", Height = 96 },
+                new Person { Name = "Steve", Height = 65 },
+                new Person { Name = "Joel", Height = 182 },
+                new Person { Name = "Bob", Height = 134 }
             };
         }
     }
