ca3a7e08deda45524a85a856aa4b9718ec5bc4a7
Naveenkumar S <naveenkumar.sanjeevirayan@syncfusion.com>
2022-06-23 12:05:11 +0530

    Updated all chart samples with WindowsAppSDK package

---
+++ PolarChartGettingStarted/PolarChartGettingStartedDesktop/Model/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolarChartGettingStartedDesktop
+{
+    public class PlantData
+    {
+        public string Direction { get; set; }
+        public double Tree { get; set; }
+    }
+}
---
+++ PolarChartGettingStarted/PolarChartGettingStartedDesktop/ViewModel/ChartViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolarChartGettingStartedDesktop
+{
+    public class ChartViewModel
+    {
+        public ObservableCollection<PlantData> PlantDetails { get; set; }
+
+        public ChartViewModel()
+        {
+            PlantDetails = new ObservableCollection<PlantData>()
+            {
+            new PlantData(){ Direction = "North", Tree = 80 },
+            new PlantData(){ Direction = "NorthWest", Tree = 87 },
+            new PlantData(){ Direction = "West", Tree = 78 },
+            new PlantData(){ Direction = "SouthWest", Tree = 85 },
+            new PlantData(){ Direction = "South", Tree = 81 },
+            new PlantData(){ Direction = "SouthEast", Tree = 88 },
+            new PlantData(){ Direction = "East", Tree = 80 },
+            new PlantData(){ Direction = "NorthEast", Tree = 85 },
+            };
+        }
+    }
+}
